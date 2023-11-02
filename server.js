9c339930a7d0de904711c5ee8b0711be7b939982
Bethvour Chike <bethvourc@gmail.com>
2023-11-02 11:27:26 -0500

    ready for test - need to improve css

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/clocking', (req, res) => {
+    const { name, action } = req.body;
+    const currentTime = new Date().toISOString();
+
+    fs.readFile('./data/timesheets.json', (err, data) => {
+        if (err) throw err;
+        let timesheets = JSON.parse(data);
+
+        if(action === 'clock_in') {
+            timesheets[name] = { ...timesheets[name], clockIn: currentTime };
+        } else if(action === 'clock_out') {
+            timesheets[name] = { ...timesheets[name], clockOut: currentTime };
+        }
+
+        fs.writeFile('./data/timesheets.json', JSON.stringify(timesheets), (err) => {
+            if (err) throw err;
+            res.json({ message: `Successfully recorded ${action.replace('_', ' ')} for ${name} at ${currentTime}` });
+        });
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
